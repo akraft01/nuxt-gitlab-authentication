f2a6f4fd78032dd88b95d446078b51b3b90b75fe
Neil Mispelaar <neil.mispelaar@statcan.gc.ca>
2024-07-31 16:09:06 -0400

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <u-container>
+      <u-card class="mt-10">
+        <template #header>
+          <div class="flex justify-between">
+            <h1>Welcome to Nuxt UI Starter</h1>
+            <color-scheme>
+              <u-select
+                v-model="$colorMode.preference"
+                :options="['system', 'light', 'dark']"
+              />
+            </color-scheme>
+          </div>
+        </template>
+        <u-button
+          icon="i-heroicons-book-open"
+          to="https://ui.nuxt.com"
+          target="_blank"
+        >
+          Open Nuxt UI Documentation
+        </u-button>
+      </u-card>
+    </u-container>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  compatibilityDate: '2024-07-17',
+});
